3dd912f32383851154b0654de9653760f44174a8
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 22:32:34 +0300

    update

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,7 +13,8 @@ class Quiz extends Component {
     }
 
     render () {
-        const isQuizEnd = (quizData.quiz_questions.length == (this.state.quiz_position -1)) 
+        
+        const isQuizEnd = (quizData.quiz_questions.length === (this.state.quiz_position -1)) 
 
         return (
             <div> 
