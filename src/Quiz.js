f1301fa1d4714cc6bcde9c397cb3321b2f2846bb
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 23:14:44 +0300

    handleClick

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,14 +12,23 @@ class Quiz extends Component {
         this.state = {quiz_position:1};
     }
 
+    showNextQuestion() {
+
+        this.state( (state) => {
+          return {quiz_position: this.state.quiz_position + 1};
+        })
+        
+    }
+
     render () {
         
-        const isQuizEnd = (this.state.quiz_position -1) == quizData.quiz_questions.length 
+        const isQuizEnd = (this.state.quiz_position -1) === quizData.quiz_questions.length 
 
         return (
             <div> 
 
-              {isQuizEnd ? ( <QuizEnd />) : (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />)}                  
+              {isQuizEnd ? ( <QuizEnd />) : (<QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} 
+                              quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />)}                  
                 
             </div>
         );
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -12,7 +12,9 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton key={index} button_text={answer_option}/> 
+                        })}
                     </ul>
                 </section>
             </main>
