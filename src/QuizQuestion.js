5507e49f3f8aaa7b31f5c97385e0a20ef0a46b89
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 23:15:18 +0300

    update

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,6 +4,16 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    
+    handleClick(buttonText) {
+
+        if (buttonText === this.props.quiz_question.answer) {
+
+            this.props.showNextQuestionHandler();
+
+        }
+    }
+
     render () {
         return (
             <main>
@@ -13,7 +23,9 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
-                            return <QuizQuestionButton key={index} button_text={answer_option}/> 
+                            return <QuizQuestionButton key={index} 
+                                    button_text={answer_option} 
+                                    clickHandler={this.handleClick.bind(this)}/> 
                         })}
                     </ul>
                 </section>
