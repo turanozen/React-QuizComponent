037f88568df5918ca01430a44894026deec1ca5c
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 16:11:45 +0300

    update

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -7,7 +7,7 @@ class QuizQuestionButton extends Component {
 
         return (
             <li>
-              <button />
+              <button> {this.props.button_text} </button>
             </li>
         );
     }
