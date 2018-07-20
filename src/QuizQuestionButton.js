afca4d0cffbd8bff0bedeea73116f063997b7579
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 16:10:09 +0300

    QuizQuestionButton created

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class QuizQuestionButton extends Component {
+
+    render() {
+
+        return (
+            <li>
+              <button />
+            </li>
+        );
+    }
+}
+
+export default QuizQuestionButton;
