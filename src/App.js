90ba903ababbc3b9aa7ef7a8a354c597a8c86dba
Turan Ozen <turan.ozen@gmail.com>
2018-07-20 15:44:13 +0300

    quiz component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+
+import Quiz from './Quiz';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
