a10878e937ebe163f4591ac23e03f13778ec1fa2
Turan Ozen <turan.ozen@gmail.com>
2018-07-21 07:35:30 +0300

    answers

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,13 +4,20 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {incorrectAnswer:false};
+    }
     
     handleClick(buttonText) {
 
         if (buttonText === this.props.quiz_question.answer) {
 
+            this.setState({incorrectAnswer:false});
             this.props.showNextQuestionHandler();
 
+        } else {
+            this.setState({incorrectAnswer:true});
         }
     }
 
@@ -29,6 +36,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error"> Maalesef yanlış cevap</p>:null}
             </main>
         )
     }
